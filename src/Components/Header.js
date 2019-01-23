e2461c8ed4b228a20c768a58af2ca150e6c9badb
IvanZ <apsent86@gmail.com>
2019-01-23 16:28:41 +0300

    add Header and User

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <div className="header-line"></div>
+            </div>
+        );
+    }
+}
+
+export default Header;
---
+++ src/Components/User.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Professions from "./Professions";
+
+class User extends Component {
+
+    constructor(props, context) {
+
+        super(props, context);
+
+        this.state={
+            firstName:'FirstName',
+            lastName:'LastName',
+            professions:[
+                'tester',
+                'finder'
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div className="user">
+                <h1>{this.state.firstName+" "+this.state.lastName}</h1>
+                <Professions data={this.state.professions} />
+            </div>
+        );
+    }
+}
+
+export default User;
